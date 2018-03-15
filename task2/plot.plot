954e571cdd4d61b2bed0fe0fd7412547b524cb5a
isgerasin <isgerasin@mail.ru>
2018-03-15 21:21:29 +0300

    v2.0

---
+++ task2/plot.plot
@@ -0,0 +1,3 @@
+f(x) = (a*x)+b
+fit f(x) "output" every ::0::7 via a,b
+plot "output"
--- task2/test.sh
+++ task2/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-while /bin/true
+MC=16
+rm output
+for ((k = 1; k < 5; k++))
 do
-	for ((i = 1; i < 19; i++))
+	for ((i = 1; i < ${MC}+1; i++))
 	do
-		echo -n "$i " >> output
-		(time -p ./a.out $i ) |& grep "real" | awk '{print 1/$2}' >> output
+		echo -n "$i, " >> output
+		(time -p ./a.out $i ) |& grep "real" | awk '{print $2}' >> output
+		echo -e "[$i / ${MC}]\e[32mDone\e[0m"
 	done
+	echo "$k"
 done
